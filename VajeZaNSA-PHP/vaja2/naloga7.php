bdaff7cde34b9ae89486af5c3fef47054a0a2d88
Gašper <gasper.nemgar@gmail.com>
2024-01-17 12:32:32 +0100

    Update naloga7.php

--- VajeZaNSA-PHP/vaja2/naloga7.php
+++ VajeZaNSA-PHP/vaja2/naloga7.php
@@ -1,5 +1,5 @@
 <?php
-  $ime = "Ime";
+  $ime = "Gašper";
   $priimek ="Priimek";
   echo "<span style=\"color:red;font-size:20px;\">Moje ime = " . $ime . "</span><br>";
   echo "<span style=\"color:blue;font-size:30px;\">Moj priimek = " . $priimek . "</span><br>"; 
