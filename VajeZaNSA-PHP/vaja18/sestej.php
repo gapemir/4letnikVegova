8f32bae90fb0973bac274da6c1e70454cb5b54bf
Gašper <gasper.nemgar@gmail.com>
2024-03-27 08:59:34 +0100

    Update sestej.php

--- VajeZaNSA-PHP/vaja18/sestej.php
+++ VajeZaNSA-PHP/vaja18/sestej.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_GET['podatek1']) && isset($_GET['podatek2']) && isset($_GET['op'])) {
-    $mysqli = new mysqli("193.2.190.23", "gasper2112", "nemga12516", "R4_Nemgar30", "3306");
+    $mysqli = new mysqli("193.2.190.23", "gasper2112", "testtesttest", "R4_Nemgar30", "3306");
     $stmt = $mysqli->prepare("INSERT INTO operacija(operacija,podatek1,podatek2) VALUES(?,?,?);");
 
     $stmt->bind_param("sii", $_GET['op'], $_GET['podatek1'], $_GET['podatek2']);
